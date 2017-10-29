87e5a36c2993e6761318a96f91c9cc52f47323f6
Dennis Addo <dennis_addo@aol.com>
2017-10-29 19:06:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 function takeANumber(arrays,person) {
-  array.push(person);
-  return `Welcome, Ada. You are number ${array.indexOf(person)} in line.`;
+  arrays.push(person);
+  return `Welcome, Ada. You are number ${arrays.indexOf(person)} in line.`;
 }
