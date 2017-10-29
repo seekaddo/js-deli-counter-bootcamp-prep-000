43303b615f035b4666f6e70aee2581d0f4a80dbb
Dennis Addo <dennis_addo@aol.com>
2017-10-29 19:10:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function takeANumber(arrays,person) {
   arrays.push(person);
   return `Welcome, Ada. You are number ${arrays.indexOf(person)} in line.`;
 }
+
+function nowServing(list) {
+  if (list.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  return list.shift();
+}
